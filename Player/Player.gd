9b0f31f50d3517d22031193933d3523354022616
Nick <nick@firedev.com>
2020-10-20 14:20:20 +0300

    Attack Animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,12 +6,23 @@ const FRICTION = 400
 
 var velocity = Vector2.ZERO
 
+enum {
+	MOVE, 
+	ROLL,
+	ATTACK,
+}
+
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active=true
 	
 func _physics_process(delta):
+	move_state(delta)
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
