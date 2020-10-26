c1e7d1b2fb541d425645b01836dedf253d021084
Nick Ostrovsky <nick@firedev.com>
2020-10-26 18:48:20 +0300

    Project Collision Layers

--- World/Grass.gd
+++ World/Grass.gd
@@ -12,8 +12,6 @@ func create_grass_effect():
 	world.add_child(grassEffect)
 	grassEffect.global_position = global_position
 
-func _on_Hurtbox_area_entered(area):
-	print(area.get_node("../").name)
-	if (area.get_node("../").name == "HitboxPivot"):
-		create_grass_effect()
-		queue_free() 
+func _on_Hurtbox_area_entered(area): 
+	create_grass_effect()
+	queue_free() 
