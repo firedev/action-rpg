c2ac854dfb9e387c588c69eb20c7e01ab17f091b
Nick Ostrovsky <nick@firedev.com>
2020-10-26 20:39:08 +0300

    Roll Animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -18,6 +18,7 @@ onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 func _ready():
+	updateAnimationTrees(Vector2.RIGHT)
 	animationTree.active=true
 	
 func _process(delta):
@@ -28,12 +29,13 @@ func _process(delta):
 			roll_state(delta)
 		ATTACK:
 			attack_state(delta)
+			
 func roll_state(delta):
-	return
+	animationState.travel("Roll")
+	velocity = move_and_slide(velocity.normalized() * MAX_SPEED)
 
 func roll_finished():
 	state = MOVE
-
 	
 func attack_state(delta):
 	velocity = Vector2.ZERO
@@ -42,6 +44,12 @@ func attack_state(delta):
 func attack_finished():
 	state = MOVE
 
+func updateAnimationTrees(input_vector):
+	animationTree.set("parameters/Idle/blend_position", input_vector)
+	animationTree.set("parameters/Run/blend_position", input_vector)
+	animationTree.set("parameters/Attack/blend_position", input_vector)
+	animationTree.set("parameters/Roll/blend_position", input_vector)
+	
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -49,10 +57,11 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
-		animationTree.set("parameters/Attack/blend_position", input_vector)
+		updateAnimationTrees(input_vector)	
 		velocity = velocity.move_toward(input_vector.normalized() * MAX_SPEED, ACCELERATION * delta)
+		
+		if Input.is_action_just_pressed("roll"):
+			state = ROLL
 		animationState.travel("Run")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -62,4 +71,3 @@ func move_state(delta):
 		state = ATTACK
 		
 	velocity = move_and_slide(velocity)
-	
--- World/Grass.gd
+++ World/Grass.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	pass
 	# if Input.is_action_just_pressed("attack"):
 
