b0f86a6b779b2c65376bf649f5e9a8d9cdfb506e
Nick Ostrovsky <nick@firedev.com>
2020-10-26 18:21:14 +0300

    Mowable Grass

--- World/Grass.gd
+++ World/Grass.gd
@@ -2,10 +2,18 @@ extends Node2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect  = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
-		queue_free()
+	pass
+	# if Input.is_action_just_pressed("attack"):
+
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+func _on_Hurtbox_area_entered(area):
+	print(area.get_node("../").name)
+	if (area.get_node("../").name == "HitboxPivot"):
+		create_grass_effect()
+		queue_free() 
