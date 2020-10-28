db4ad14e42c8a4889c2ca0006b26bb3e787e1a5b
Nick Ostrovsky <nick@firedev.com>
2020-10-28 15:50:56 +0300

    Shared Health

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -4,14 +4,18 @@ const FRICTION = 200
 const KNOCKBACK_SPEED = 100
 
 var knockback = Vector2.ZERO
-var health = 3
+onready var stats = $Stats
+
+func _ready():
+	print(stats.max_health)
+	print(stats.health)
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
-	if (knockback == Vector2.ZERO && health <= 0): 
+	if (knockback == Vector2.ZERO && stats.health <= 0): 
 		queue_free()
 
 func _on_Hurtbox_area_entered(area):
-	health = health - 1
+	stats.health = stats.health - 1
 	knockback = area.knockback_vector * KNOCKBACK_SPEED
