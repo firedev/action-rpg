07eb1980d0b542b275d1f6409fbec25316b005cf
Nick <nick@firedev.com>
2020-10-31 12:49:06 +0300

    Chasing Bats

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,12 +1,17 @@
 extends KinematicBody2D
 
 const FRICTION = 200
-const KNOCKBACK_SPEED = 100
+const KNOCKBACK_SPEED = 200
+const MAX_SPEED = 80
+const ACCELERATION = 250
 
 var knockback = Vector2.ZERO
 var velocity  = Vector2.ZERO
 var dead = false
+
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var sprite = $Bat
 
 const Effect = preload("res://Effects/EnemyDeathEffect.tscn")
 
@@ -33,12 +38,25 @@ func _physics_process(delta):
 	
 	match(state):
 		IDLE:
-			pass
+			velocity = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
 		CHASE:
-			pass
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity =  velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+	
 		WANDER:
 			pass
-	
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
+			
 func _on_Hurtbox_area_entered(area):
 	stats.register_hit(area.damage)
 	knockback = area.knockback_vector * KNOCKBACK_SPEED
