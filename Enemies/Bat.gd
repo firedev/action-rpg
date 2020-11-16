4c8b7165ad8457e98395b538a232cab45b8d22bc
Nick Ostrovsky <nick@firedev.com>
2020-11-16 19:28:47 +0300

    Randomized Behavior

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -4,10 +4,12 @@ const FRICTION = 200
 const KNOCKBACK_SPEED = 200
 const MAX_SPEED = 80
 const ACCELERATION = 250
+const WANDER_RANGE = 4
 
 var knockback = Vector2.ZERO
 var velocity  = Vector2.ZERO
 var dead = false
+var state = IDLE
 
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
@@ -24,7 +26,8 @@ enum {
 	WANDER,
 }
 
-var state = IDLE 
+func _ready():
+	randomize_behavior()
 
 func create_effect():
 	var effect = Effect.instance()
@@ -44,28 +47,34 @@ func _physics_process(delta):
 			velocity = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
 			if wanderController.get_time_left() == 0:
-				state = pick_random_state([IDLE, WANDER])
-				wanderController.start_wander_timer(rand_range(1,3))
+				randomize_behavior()
 
 		WANDER:
 			seek_player()
+			accelerate_towards_point(wanderController.target_position, delta)
+			
 			if wanderController.get_time_left() == 0:
-				state = pick_random_state([IDLE, WANDER])
-				wanderController.start_wander_timer(rand_range(1,3))
-			var direction = global_position.direction_to(wanderController.target_position)
-			velocity =  velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				randomize_behavior()
+
+			if global_position.distance_to(wanderController.target_position) <= WANDER_RANGE:
+				randomize_behavior()
+			
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
-				var direction = global_position.direction_to(player.global_position)
-				velocity =  velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				accelerate_towards_point(player.global_position, delta)
 			else:
 				state = IDLE
-			sprite.flip_h = velocity.x < 0
 
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * KNOCKBACK_SPEED * 2
 	velocity = move_and_slide(velocity)
+
+func accelerate_towards_point(point, delta):
+	var direction = global_position.direction_to(point)
+	velocity =  velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+			
 
 func seek_player():
 	if playerDetectionZone.can_see_player():
@@ -75,6 +84,10 @@ func _on_Hurtbox_area_entered(area):
 	stats.register_hit(area.damage)
 	knockback = area.knockback_vector * KNOCKBACK_SPEED
 	hurtbox.create_hit_effect()
+
+func randomize_behavior():
+	state = pick_random_state([ IDLE, WANDER ])
+	wanderController.start_wander_timer(rand_range(1,3))
 
 func pick_random_state(state_list):
 	state_list.shuffle()
